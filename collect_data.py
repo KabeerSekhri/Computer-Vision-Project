77a2524d1eb4934558cb9fda36b2a6c347af9a01
Your Name <you@example.com>
2024-11-10 19:02:20 +0530

    made capturing function

--- collect_data.py
+++ collect_data.py
@@ -1,42 +1,48 @@
 import os
 import cv2
 
-# Right now we can define the no. of signs and no. of imgs for each sign we need to collect
-# However, we need to collect all of them at once which is not optimal incase of mistakes/additions
-# Need to make it so we can specify the sign we want to capture (to make data storage clearer)
-# and then run it just once for that so anyone can add a sign to it, make it a function maybe.
-
+# How To Use:
+# Enter the character/word for which the sign language has to be captured
+# After pressing 'Spacebar' the camera captures 100 images
 
 DATA_DIR = './data'
 if not os.path.exists(DATA_DIR): # Makes a directory 'data' if not existing
     os.makedirs(DATA_DIR)
 
-number_of_classes = 1 # No. of signs to capture
-dataset_size = 100 # No. of imgs to capture
 
-cap = cv2.VideoCapture(0) 
-for j in range(number_of_classes):
-    if not os.path.exists(os.path.join(DATA_DIR, str(j))): # Make directory inside 'data' labeled j
-        os.makedirs(os.path.join(DATA_DIR, str(j)))
+def capture_sign(sign, dataset_size = 100):
+    cap = cv2.VideoCapture(0) 
 
-    print('Collecting data for class {}'.format(j))
 
-    done = False
+    if not os.path.exists(os.path.join(DATA_DIR, str(sign))): # Make directory inside 'data' labeled j
+        os.makedirs(os.path.join(DATA_DIR, str(sign)))
+
+    print('Collecting data for class {}'.format(sign))
+
     while True: # Used to allow user to be prepared for capture
         ret, frame = cap.read() 
-        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3, cv2.LINE_AA) 
+        cv2.putText(frame, 'Press SpaceBar to begin capture', (100, 50), cv2.FONT_HERSHEY_PLAIN, 1.3, (255, 255, 255), 3, cv2.LINE_AA) 
         cv2.imshow('frame', frame) # Start capture
-        if cv2.waitKey(25) == ord('q'):
+        if cv2.waitKey(1) & 0xFF == 32:
             break
 
     counter = 0
     while counter < dataset_size:
-        ret, frame = cap.read()
+        success, frame = cap.read()
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
-        cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame) # Store captured frame with appr. name
+        cv2.imwrite(os.path.join(DATA_DIR, str(sign), '{}.jpg'.format(counter)), frame) # Store captured frame with apprp. name
 
         counter += 1
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
+
+sign = ""
+while not sign.strip():  # Continue looping while the input is empty
+    sign = input("Enter the sign to capture: ")
+    if not sign.strip():
+        print("Input cannot be empty. Try again.")
+
+
+capture_sign(sign)
