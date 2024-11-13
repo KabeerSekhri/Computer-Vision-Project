e144bda86ed5cb20b3443be017ff05981c71a3bf
Your Name <you@example.com>
2024-11-13 19:51:32 +0530

    added data from landmarks to a pickle file' '

--- create_dataset.py
+++ create_dataset.py
@@ -2,6 +2,7 @@ import os
 import cv2
 import mediapipe as mp
 import matplotlib.pyplot as plt
+import pickle
 
 mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
@@ -9,9 +10,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence = 0.3) # Track hands only in static frame not across all (if confidence above threshold)
 
-
-
 DATA_DIR = './data'
+
+data = []
+labels = []
 
 for dir in os.listdir(DATA_DIR): # Go through each directory (sign)
     dir_path = os.path.join(DATA_DIR, dir) 
@@ -28,16 +30,20 @@ for dir in os.listdir(DATA_DIR): # Go through each directory (sign)
 
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        data_sub = [] # Create an array with all XY coordinates of all landmarks of all hands in an image
         results = hands.process(img_rgb)
-        if results.multi_hand_landmarks:
-            for hand_landmarks in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(
-                    img_rgb, hand_landmarks, mp_hands.HAND_CONNECTIONS,
-                    mp_drawing_styles.get_default_hand_landmarks_style(),
-                    mp_drawing_styles.get_default_hand_connections_style()
-                )
+        if results.multi_hand_landmarks: 
+            for hand_landmarks in results.multi_hand_landmarks: # Go through each hand tracked/captured
+                for i in range(len(hand_landmarks.landmark)): # For each landmark in image get X and Y coordinate
+                    x = hand_landmarks.landmark[i].x
+                    y = hand_landmarks.landmark[i].y
 
-        plt.figure()
-        plt.imshow(img_rgb)
+                    data_sub.append(x)
+                    data_sub.append(y)
+            
+            data.append(data_sub)
+            labels.append(dir)
 
-plt.show()
+f = open('data.pickle', 'wb') # Create a file to store data
+pickle.dump({'data': data, 'labels': labels}, f)
+f.close()
